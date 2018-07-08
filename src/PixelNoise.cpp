8ec2931454946baa5fe45283e89a2555b07680d8
Alexander Elfimow <my.free.mail@gmx.de>
2018-07-08 23:50:06 +0200

    Added file

---
+++ src/PixelNoise.cpp
@@ -0,0 +1,17 @@
+#include "PixelNoise.h"
+
+PixelNoise::~PixelNoise()
+{
+}
+
+void PixelNoise::Create()
+{
+}
+
+void PixelNoise::Draw()
+{
+}
+
+void PixelNoise::Destroy()
+{
+}
---
+++ src/PixelNoise.h
@@ -0,0 +1,24 @@
+#ifdef PIXEL_NOISE_H
+#error Already included
+#else
+#define PIXEL_NOISE_H
+
+class PixelNoise
+{
+    public:
+        ~PixelNoise();
+
+    public:
+        static void Create();
+        static void Draw();
+        static void Destroy();
+
+    public:
+        PixelNoise() = delete;
+        PixelNoise(const PixelNoise &instance) = delete;
+        PixelNoise(const PixelNoise &&instance) = delete;
+        PixelNoise &operator=(const PixelNoise &instance) = delete;
+        PixelNoise &operator=(const PixelNoise &&instance) = delete;
+};
+
+#endif
